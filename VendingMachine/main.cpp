e55bee641fb425a1b2ff8e903bb43ddea0ca286c
McKenzie Humann <mhv3p@mail.umkc.edu>
2019-10-02 17:05:54 -0500

    Initial Commit

---
+++ VendingMachine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VendingMachine
+//
+//  Created by McKenzie Humann on 10/2/19.
+//  Copyright © 2019 McKenzie Humann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
